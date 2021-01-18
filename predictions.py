3af235b3931f18b0bef76846daedd73dbbb3363d
Alessandro Alterno <alternonolimit@gmail.com>
2021-01-18 02:31:27 +0100

    Can now get fixtures by championshipRound and generate JSON for each championshipRound

--- predictions.py
+++ predictions.py
@@ -1,30 +1,66 @@
 import requests
+import pandas as pd
 import json
 
-HEADERS = { 'X-RapidAPI-Key': ''}
 HOST = "https://api-football-v1.p.rapidapi.com/v2/"
+LIGUE1_ID = "2664"
+ROUND_LABEL = "Regular_Season_-_"
 
-# - On requete 1 journée de championnat
-# - On récupère tous les fixtures ID (10 normalement) + vainqueur du match 
-# - On requête les prédictions pour chaque fixture ID récupéré 
-# - On ajoute les données reçues, ainsi que le vainqueur du match dans le CSV
+def getApiKey():
+    f = open("vars", "r")
+    return f.readline()
+
+HEADERS = { 'X-RapidAPI-Key': getApiKey()}
 
 
+# 1 - On requete 1 journée de championnat
+# 2 - On récupère tous les fixtures ID (10 normalement) + vainqueur du match 
+# 3 - On requête les prédictions pour chaque fixture ID récupéré 
+# 4 - On ajoute les données reçues, ainsi que le vainqueur du match dans le CSV
+
+def createJsonFile(dictionary, championshipRound):
+    with open("/fixtures/" + str(championshipRound) + ".json", "w") as outfile:
+        json.dump(dictionary, outfile)
+    
+
+def getWinner(homeTeamScore, awayTeamScore):
+    if homeTeamScore > awayTeamScore:
+        return 1
+    elif homeTeamScore < awayTeamScore:
+        return 2
+    else:
+        return 0
+
+def getMatchesForChampionshipRound(championshipRound):
+    url = HOST + "fixtures/league/" + LIGUE1_ID + "/" + ROUND_LABEL + str(championshipRound - 1)
+    print("URL => " + url)
+    req = requests.get(url, headers = HEADERS)
+    reqJson = json.loads(req.text)
+
+    fixtures = reqJson["api"]["fixtures"]
+    fixturesIds = {}
+    for fixture in fixtures:
+        fixtureWinner = getWinner(fixture["goalsHomeTeam"], fixture["goalsAwayTeam"])
+        
+        fixturesIds[fixture["fixture_id"]] = fixtureWinner
+    
+    createJsonFile(fixturesIds, championshipRound-1)
 
 def getPredictionsForFicture(fixtureId):
+    url = HOST + "predictions/" + fixtureId
+    req = requests.get(url, headers = HEADERS)
+    reqJson = json.loads(req.text)
 
-    req = requests.get(HOST + "predictions/" + fixtureId, headers = HEADERS)
-    req_json = json.loads(req.text)
+    homeTeamStats = reqJson["api"]["predictions"][0]["teams"]["home"]["last_5_matches"]
+    awayTeamStats = reqJson["api"]["predictions"][0]["teams"]["away"]["last_5_matches"]
+    comparison = reqJson["api"]["predictions"][0]["comparison"]["h2h"]
 
-    home_team_stats = req_json["api"]["predictions"][0]["teams"]["home"]["last_5_matches"]
-    away_team_stats = req_json["api"]["predictions"][0]["teams"]["away"]["last_5_matches"]
-    comparison = req_json["api"]["predictions"][0]["comparison"]["h2h"]
-
-    return  home_team_stats, away_team_stats, comparison
+    return  homeTeamStats, awayTeamStats, comparison
 
 if __name__ == "__main__":
-    home_team_stats, away_team_stats, comparison = getLastFiveMatchStats("571659")
-
-    print(home_team_stats)
-    print(away_team_stats)
-    print(comparison)
+    # homeTeamStats, awayTeamStats, comparison = getLastFiveMatchStats("571659")
+    # print(homeTeamStats)
+    # print(awayTeamStats)
+    # print(comparison)
+    getMatchesForChampionshipRound(20)
+    
