ff56e3e3b240ef8256542ba2544292ade7eb2f3b
Alessandro Alterno <alternonolimit@gmail.com>
2021-01-19 13:51:32 +0100

    Added first steps to read user inputs as Int

--- predictions.py
+++ predictions.py
@@ -1,4 +1,20 @@
 import generation as gen
+import asyncio
+
+chosenChampionshipDay = 0
+
+def askForIntValue():
+    isInputIntValue = False
+    while isInputIntValue == False:
+        try:
+            chosenChampionshipDay = int(input("First, tell us for what day you want to know a result ! Choose from 1 to 34\n"))
+            print("chosenday ", chosenChampionshipDay)
+            gen.generateJsonByChampionshipRound(chosenChampionshipDay)
+            isInputIntValue = True
+        except ValueError:
+            print("please enter a number between 1 and 34")
+            isInputIntValue = False
 
 if __name__ == "__main__":
-    print("Hello ! ")
+    print("Hello ! Let's predict the result of a match !")
+    askForIntValue()
